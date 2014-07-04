58214dee9f40a2c190b0fc9b620f35ad10f9290d
Ivan Nemytchenko <nemytchenko@gmail.com>
2014-07-04 22:31:24 +0700

    implemented more of assignment API

--- assignment_app.rb
+++ assignment_app.rb
@@ -1,32 +1,54 @@
 require 'sinatra'
 require 'sinatra/json'
+require 'faker'
 require_relative 'models'
 
 def authenticate!(phone, pwd)
   p = Passenger.find_by_phone(phone)
   unless p.password == pwd
-    status 403 && json 'Not authorized'
+    status 403 and json('Not authorized')
   end
   p
 end
 
-post '/booking/create/:phone' do
+get '/booking/create/:phone' do
+  passenger = authenticate!(params[:phone], params[:pwd])
+  point = Point.new
 
+  booking = Booking.create(:passenger_id => passenger.id, :lat => point.lat, :lon => point.lon)
+  json booking.id
 end
 
 get '/booking/:phone' do
+  passenger = authenticate!(params[:phone], params[:pwd])
 
+  booking = Booking.find_all_by_passenger_id(passenger.id).last
+  json booking.id
 end
 
 get '/booking/id/:phone' do
+  passenger = authenticate!(params[:phone], params[:pwd])
 
+  booking = Booking.find_all_by_passenger_id(passenger.id).last
+  json booking.id
 end
 
 get '/booking/details/:phone' do
+  passenger = authenticate!(params[:phone], params[:pwd])
 
+  booking = Booking.find_all_by_passenger_id(passenger.id).last
+  json booking
 end
 
 get '/assignment/new/:booking_id' do
   lat, long = params[:lat], params[:long]
 
+end
+
+class Point
+  attr_accessor :lat, :lon
+  def initialize
+    @lat = Faker::Address.latitude
+    @lon = Faker::Address.longitude
+  end
 end
