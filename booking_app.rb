3f5163269010554cd78f491c5c1a6003bcec2f31
Ivan Nemytchenko <nemytchenko@gmail.com>
2014-07-04 21:46:15 +0700

    Sinatra app actually uses AR model

--- booking_app.rb
+++ booking_app.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
+require 'sinatra/json'
 
+require_relative 'passenger_creator'
 
 get '/passenger/new/:phone' do
+  creator = PassengerCreator.new
+  passenger = creator.create(params[:phone])
 
+  json passenger.id
 end
 
 get '/pwd/:phone' do
