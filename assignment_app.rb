b81fd4d0ef211c99392f7430fd7f663ee03d0385
Ivan Nemytchenko <nemytchenko@gmail.com>
2014-07-04 20:31:14 +0700

    APIs skeleton

---
+++ assignment_app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+post '/booking/create/:phone' do
+
+end
+
+get '/booking/:phone' do
+
+end
+
+get '/booking/id/:phone' do
+
+end
+
+get '/booking/details/:phone' do
+
+end
---
+++ booking_app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+
+
+get '/passenger/new/:phone' do
+
+end
+
+get '/pwd/:phone' do
+
+end
+
+get '/assignment/new' do
+  lat, long = params[:lat], params[:long]
+end
