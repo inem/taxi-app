23d8c4bcfc337620b1f3377776e700511f733abc
Ivan Nemytchenko <nemytchenko@gmail.com>
2014-07-04 21:32:44 +0700

    PassengerCreator

---
+++ passenger.rb
@@ -0,0 +1,22 @@
+require "sequel"
+require_relative "db"
+
+class PassengerCreator
+  def new(klass = Passenger)
+    @klass = klass
+  end
+
+  def create(phone)
+    passenger = @klass.find(phone)
+    unless passenger
+      passenger = @klass.create(phone: phone, password: generate_password)
+    end
+
+    passenger
+  end
+
+  private
+  def generate_password
+    (0...8).map { ('a'..'z').to_a[rand(26)] }.join
+  end
+end
